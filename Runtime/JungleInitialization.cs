dd33810b990003315e4776cc24abd6a2c86268d9
Jack Randolph <techjack457@gmail.com>
2023-04-23 22:35:27 -0400

    Automatic singleton initialization

---
+++ Runtime/JungleInitialization.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Jungle
+{
+    public static class JungleInitialization
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void BeforeSceneLoadCallback()
+        {
+            var jungleRuntimeGameObject = new GameObject("[Jungle Runtime]");
+            jungleRuntimeGameObject.AddComponent<JungleRuntime>();
+            Object.DontDestroyOnLoad(jungleRuntimeGameObject);
+        }
+    }
+}
